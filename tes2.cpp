67312a1bdcbf2e7f1778a726c9d2395ef893a8f9
joshuasir <jckryder123@gmail.com>
2020-09-25 18:03:17 +0700

    master

--- tes2.cpp
+++ tes2.cpp
@@ -1,16 +1,18 @@
 #include <stdio.h>
 #include <unistd.h>
 int main(){
-    for(int i=0;i<7;i++){
-       usleep(6000);
+    for(int i=0;i<20;i++){
+       usleep(70000);
         printf("N");
-        printf("YEET\n");
-       usleep(6000);
+        //printf("YEET\n");
+       usleep(70000);
         printf("A");
-       usleep(6000);
+       usleep(70000);
+      // puts("");
     }
-    usleep(60000);
-    printf("BATMAN\n");
+    puts("");
+    usleep(600000000);
+    printf("BATMAN!\n");
     getchar();
     return 0;
 }
